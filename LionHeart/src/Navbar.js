318b4d0ffe9175074d742c2188be2c91763c81ac
JohnFull30 <johnathanfuller0@gmail.com>
2022-07-26 13:05:18 -0400

    finished making and styling the nav bar

--- LionHeart/src/Navbar.js
+++ LionHeart/src/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { withStyles } from "@mui/styles";
+import { IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
@@ -6,17 +8,54 @@ import ExploreRoundedIcon from "@mui/icons-material/ExploreRounded";
 import ChatBubbleRoundedIcon from "@mui/icons-material/ChatBubbleRounded";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
-const Navbar = () => {
+const styles = {
+  root: {
+    "& .appBar": {
+      position: "fixed",
+      backgroundColor: "#ffffff",
+      alignItems: "center",
+    },
+
+    "& .toolBar": {
+      position: "fixed",
+      bottom: "0px",
+    },
+
+    "& .toolBar .MuiIconButton-label": {
+      color: "#A7B5F9",
+      display: "flex",
+      flexDirection: "column",
+      padding: "0px 96px 0px 96px",
+      fontSize: "medium",
+    },
+  },
+};
+
+const Navbar = ({ classes }) => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <HomeRoundedIcon></HomeRoundedIcon>
-        <ExploreRoundedIcon></ExploreRoundedIcon>
-        <ChatBubbleRoundedIcon></ChatBubbleRoundedIcon>
-        <MenuRoundedIcon></MenuRoundedIcon>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar className="appBar">
+        <Toolbar className="toolBar">
+          <IconButton>
+            <HomeRoundedIcon />
+            <div>Home</div>
+          </IconButton>
+          <IconButton>
+            <ExploreRoundedIcon />
+            <div>Explore</div>
+          </IconButton>
+          <IconButton>
+            <ChatBubbleRoundedIcon />
+            <div>Chat</div>
+          </IconButton>
+          <IconButton>
+            <MenuRoundedIcon />
+            <div>Menu</div>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
